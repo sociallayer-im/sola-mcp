8f9942eea003b00ecc42d561a9b8adcbafd273ae
jiangplus <jiang.plus.times@gmail.com>
2025-05-28 07:20:15 -0700

    add prettier

--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
-import express from "express";
-import { randomUUID } from "node:crypto";
-import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js"
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
+import express from 'express';
+import { randomUUID } from 'node:crypto';
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import { isInitializeRequest } from '@modelcontextprotocol/sdk/types.js';
+import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
 
+
+// Create an MCP server
+const server = new McpServer({
+  name: 'Sola',
+  version: '1.0.0',
+  description: 'MCP server for Social Layer',
+  license: 'MIT',
+  author: 'jiangplus',
+});
+
+// Add an addition tool
+server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => {
+  console.log('add', a, b);
+  return {
+    content: [{ type: 'text', text: String(a + b) }],
+  };
+});
+
+// Add a dynamic greeting resource
+server.resource(
+  'greeting',
+  new ResourceTemplate('greeting://{name}', { list: undefined }),
+  async (uri, { name }) => ({
+    contents: [
+      {
+        uri: uri.href,
+        text: `Hello, ${name}!`,
+      },
+    ],
+  }),
+);
 
 const app = express();
 app.use(express.json());
@@ -29,7 +60,7 @@ app.post('/mcp', async (req: express.Request, res: express.Response) => {
       onsessioninitialized: (sessionId) => {
         // Store the transport by session ID
         transports[sessionId] = transport;
-      }
+      },
     });
 
     // Clean up transport when closed
@@ -39,35 +70,6 @@ app.post('/mcp', async (req: express.Request, res: express.Response) => {
       }
     };
 
-
-// Create an MCP server
-const server = new McpServer({
-  name: "Sola",
-  version: "1.0.0",
-  description: "MCP server for Social Layer",
-  license: "MIT",
-  author: "jiangplus",
-});
-
-// Add an addition tool
-server.tool("add",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
-  })
-);
-
-// Add a dynamic greeting resource
-server.resource(
-  "greeting",
-  new ResourceTemplate("greeting://{name}", { list: undefined }),
-  async (uri, { name }) => ({
-    contents: [{
-      uri: uri.href,
-      text: `Hello, ${name}!`
-    }]
-  })
-);
 
     // Connect to the MCP server
     await server.connect(transport);
